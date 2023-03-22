695d0e7986eaf674a71e38877db2378d1e0a3cd5
Poojakadam23 <128593334+Poojakadam23@users.noreply.github.com>
2023-03-22 19:08:15 +0530

    Add files via upload

---
+++ Lab Assigment_2/Hollow_3.java
@@ -0,0 +1,14 @@
+class Hollow_3{
+public static void main(String args[]){
+for(int i=6;i>=1;i--){
+  for(int j=1;j<=i;j++){
+    if(j==1||i==6||i==j)
+	 System.out.print("*");
+	 else
+	  System.out.print(" ");
+	  }
+	   System.out.println();
+
+}	 
+}
+}
---
+++ Lab Assigment_2/Pyramid_7.java
@@ -0,0 +1,16 @@
+class Pyramid_7{
+public static void main(String args[]){
+for(int i=9;i>=1;i--)
+{
+ for(int j=1;j<=i;j++)
+ {
+   System.out.print(" ");
+ }
+ for(int k=9;k>=i;k--){
+ System.out.print(i+" ");
+      
+ }
+ System.out.println();
+}
+}
+}
